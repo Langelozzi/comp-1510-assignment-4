7030994bf3b74b8fa2aa18641f83981c162f8a5b
Lucas Angelozzi <lucasangelozzi32@gmail.com>
2022-11-20 13:59:43 -0800

    create function to print character in correct spot

--- printing.py
+++ printing.py
@@ -1,4 +1,4 @@
-def print_in_color(value, color):
+def print_in_color(value, color: str, end: str = "\n"):
     colors = {
         "purple": '\033[95m',
         "blue": '\033[94m',
@@ -9,7 +9,37 @@ def print_in_color(value, color):
         "end_color": '\033[0m',
     }
 
-    print(colors[color] + str(value) + colors["end_color"])
+    print(colors[color] + str(value) + colors["end_color"], end=end)
 
 
+def print_board(x_coord, y_coord):
+    #board = """
+    # ---------------------------| |
+    # | || || || || || || || || || |
+    # | || || || || || || || || || |
+    # | || || || || || || || || || |
+    # | || || || || || || || || || |
+    # | || || || || || || || || || |
+    # | || || || || || || || || || |
+    # | || || || || || || || || || |
+    # | || || || || || || || || || |
+    # | || || || || || || || || || |
+    # | || || || || || || || || || |
+    # | |---------------------------
+    # """
 
+    for y in range(11, -1, -1):
+        for x in range(1, 11, 1):
+            if (y == 11 and x != 10) or (y == 0 and x != 1):
+                print('---', end="")
+            elif y == y_coord and x == x_coord:
+                print('|', end="")
+                print_in_color("#", "purple", end="")
+                print('|', end="")
+            else:
+                print('| |', end="")
+
+        print()
+
+
+print_board(1, 1)
