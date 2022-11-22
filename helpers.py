b886d898b3c7761d711cb49b753933597d03c75c
Lucas Angelozzi <lucasangelozzi32@gmail.com>
2022-11-21 21:31:36 -0800

    Create function for getting user choice

--- helpers.py
+++ helpers.py
@@ -1,4 +1,11 @@
+from printing import print_in_color
+
 
 
 def cleanse(text: str) -> str:
     return text.lower().strip()
+
+
+def get_user_choice() -> str:
+    print_in_color("Which direction wouldst thee liketh to traverse?", "purple")
+    return cleanse(input())
