512ef5a371af808071196c88ca17a0898388e7bb
Lucas Angelozzi <lucasangelozzi32@gmail.com>
2022-11-21 21:32:38 -0800

    Move is_valid_move to board.py

--- board.py
+++ board.py
@@ -12,18 +12,18 @@
 | || || || || || || || || || |
 | |---------------------------
 """
-import random
+from printing import print_in_color
 
 
-def some_func():
-    print("Complete the challenge")
+def test_challenge():
+    print("this is a test challenge")
 
 
 def make_board(rows, columns) -> dict:
     board = {
         (1, 0): {
             "description": "This is where you start lol",
-            "challenge": some_func,
+            "action": test_challenge,
             "solved": False,
             "north": (1, 1),
             "east": None,
@@ -32,6 +32,7 @@ def make_board(rows, columns) -> dict:
         },
         (10, 11): {
             "description": "This is how you get to the boss fight",
+            "action": test_challenge,
             "solved": False,
             "north": None,
             "east": None,
@@ -42,28 +43,39 @@ def make_board(rows, columns) -> dict:
 
     # a list of tuples where tuple[0] = description of tile, tuple[1] = challenge for that tile as a function
 
-    for x in range(1, columns + 1):
-        for y in range(1, rows + 1):
-            board[(x, y)] = {
-                "description": # function from challenge.py,
-                "challenge": # function from challenge.py,
-                "solved": False,
-                "north": (x, y+1),
-                "east": (x+1, y),
-                "south": (x, y-1),
-                "west": (x-1, y)
-            }
-
-            if x == 1:
-                board[(x, y)]["west"] = None
-            if x == 10:
-                board[(x, y)]["east"] = None
-            if y == 1 and x != 1:
-                board[(x, y)]["south"] = None
-            if y == 10 and x != 10:
-                board[(x, y)]["north"] = None
+    # for x in range(1, columns + 1):
+    #     for y in range(1, rows + 1):
+    #         board[(x, y)] = {
+    #             "description": # function from challenge.py,
+    #             "action": # function from challenge.py,
+    #             "solved": False,
+    #             "north": (x, y+1),
+    #             "east": (x+1, y),
+    #             "south": (x, y-1),
+    #             "west": (x-1, y)
+    #         }
+    #
+    #         if x == 1:
+    #             board[(x, y)]["west"] = None
+    #         if x == 10:
+    #             board[(x, y)]["east"] = None
+    #         if y == 1 and x != 1:
+    #             board[(x, y)]["south"] = None
+    #         if y == 10 and x != 10:
+    #             board[(x, y)]["north"] = None
 
     return board
+
+
+def describe_current_location(board: dict, current_position: tuple) -> None:
+    print_in_color(board[current_position]["description"], "cyan")
+
+
+def is_valid_move(direction: str, board: dict, current_position: tuple) -> bool:
+    if board[current_position][direction] is None:
+        return False
+    else:
+        return True
 
 
 def main():
@@ -71,9 +83,7 @@ def main():
     columns = 10
 
     board = make_board(rows, columns)
-    print(board[(5, 5)])
-    print(board[(5, 5)]["description"])
-    board[(5, 5)]["challenge"]()
+    describe_current_location(board, (1, 0))
 
 
 if __name__ == '__main__':
