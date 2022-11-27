b75521e5f85fc9289e65e3600c9a7b4a051b4e3d
Lucas Angelozzi <lucasangelozzi32@gmail.com>
2022-11-27 10:51:59 -0800

    add boss room to list of coords not to randomize

--- board.py
+++ board.py
@@ -46,7 +46,7 @@ def make_board(rows, columns) -> dict:
 
     for x in range(1, columns + 1):
         for y in range(1, rows + 1):
-            if x == 1 and y == 1:
+            if (x == 1 and y == 1) or (x == 10 and y == 11):
                 continue
 
             board[(x, y)] = {
