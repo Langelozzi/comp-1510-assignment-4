1f5af0118cae870d323cb614cf0d64ec05c9ceac
Amir Eskandari <113070116+am-eskandari@users.noreply.github.com>
2022-12-01 03:02:14 -0800

    Update test_move_character.py

--- tests/test_move_character.py
+++ tests/test_move_character.py
@@ -3,24 +3,79 @@ from character import move_character
 
 
 class TestMoveCharacter(TestCase):
-    board = {
-        (10, 11): {
-            "description": "God-King Thompson, the God Slayer",
-            "action": None,
-            "solved": True,
-            "directions": {
-                "north": None,
-                "east": None,
-                "south": (10, 10),
-                "west": None
-            }
-        }
-    }
-
-    character_test = {"position": (1, 1)}
 
     def test_move_character_north(self):
+        board_test = {
+            (0, 0): {
+                "directions": {
+                    "north": (0, 1),
+                    "east": (1, 0),
+                    "south": None,
+                    "west": None
+                }
+            }
+        }
 
-        actual = move_character('north')
-        expected = 
-        self.fail()
+        character_test = {"position": (0, 0)}
+
+        move_character("north", board_test, character_test)
+        actual = character_test["position"]
+        expected = (0, 1)
+        self.assertEqual(expected, actual)
+
+    def test_move_character_east(self):
+        board_test = {
+            (0, 0): {
+                "directions": {
+                    "north": (0, 1),
+                    "east": (1, 0),
+                    "south": None,
+                    "west": None
+                }
+            }
+        }
+
+        character_test = {"position": (0, 0)}
+
+        move_character("east", board_test, character_test)
+        actual = character_test["position"]
+        expected = (1, 0)
+        self.assertEqual(expected, actual)
+
+    def test_move_character_south(self):
+        board_test = {
+            (10, 10): {
+                "directions": {
+                    "north": None,
+                    "east": None,
+                    "south": (10, 9),
+                    "west": (9, 10)
+                }
+            }
+        }
+
+        character_test = {"position": (10, 10)}
+
+        move_character("south", board_test, character_test)
+        actual = character_test["position"]
+        expected = (10, 9)
+        self.assertEqual(expected, actual)
+
+    def test_move_character_west(self):
+        board_test = {
+            (10, 10): {
+                "directions": {
+                    "north": None,
+                    "east": None,
+                    "south": (10, 9),
+                    "west": (9, 10)
+                }
+            }
+        }
+
+        character_test = {"position": (10, 10)}
+
+        move_character("west", board_test, character_test)
+        actual = character_test["position"]
+        expected = (9, 10)
+        self.assertEqual(expected, actual)
