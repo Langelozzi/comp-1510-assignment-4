36af273024766a911e56cb138495d63ea801d069
Amir Eskandari <113070116+am-eskandari@users.noreply.github.com>
2022-12-01 02:47:09 -0800

    Update test_move_character.py

--- tests/test_move_character.py
+++ tests/test_move_character.py
@@ -1,7 +1,26 @@
 from unittest import TestCase
+from character import move_character
 
 
 class TestMoveCharacter(TestCase):
+    board = {
+        (10, 11): {
+            "description": "God-King Thompson, the God Slayer",
+            "action": None,
+            "solved": True,
+            "directions": {
+                "north": None,
+                "east": None,
+                "south": (10, 10),
+                "west": None
+            }
+        }
+    }
 
-    def test_move_character(self):
+    character_test = {"position": (1, 1)}
+
+    def test_move_character_north(self):
+
+        actual = move_character('north')
+        expected = 
         self.fail()
