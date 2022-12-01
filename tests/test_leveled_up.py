ec936dca51f2b9830a28f69b8c4901cf3e056664
Amir Eskandari <113070116+am-eskandari@users.noreply.github.com>
2022-12-01 03:18:11 -0800

    Create test_leveled_up.py

---
+++ tests/test_leveled_up.py
@@ -0,0 +1,29 @@
+from unittest import TestCase
+from character import leveled_up
+
+
+class TestLeveledUp(TestCase):
+    def test_leveled_up_xp_sixty_one_and_level_three(self):
+        character_test = {"xp": 61, "level": 3}
+        actual = leveled_up(character_test)
+        expected = False
+        self.assertFalse(expected, actual)
+
+    def test_leveled_up_xp_sixty_and_level_three(self):
+        character_test = {"xp": 60, "level": 3}
+        actual = leveled_up(character_test)
+        expected = False
+        self.assertFalse(expected, actual)
+
+    def test_leveled_up_xp_sixty_one_and_level_two(self):
+        character_test = {"xp": 61, "level": 2}
+        actual = leveled_up(character_test)
+        expected = False
+        self.assertFalse(expected, actual)
+
+    def test_leveled_up_xp_fiftynine_and_level_two(self):
+        character_test = {"xp": 59, "level": 2}
+        actual = leveled_up(character_test)
+        expected = True
+        self.assertTrue(expected, actual)
+        
