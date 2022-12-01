45c1a439a935c5203418aff5a24b9517088086c4
Amir Eskandari <113070116+am-eskandari@users.noreply.github.com>
2022-12-01 02:28:55 -0800

    Update test_make_character.py

--- tests/test_make_character.py
+++ tests/test_make_character.py
@@ -2,23 +2,27 @@ from unittest import TestCase
 from character import make_character
 
 
-class Test(TestCase):
+class TestMakeCharacter(TestCase):
     def test_make_character_empty_string(self):
         actual = make_character("")
-        expected = {'name': '', 'position': (1, 1), 'max_hp': 100, 'current_hp': 100, 'xp': 0, 'damage': 20, 'level': 1, 'abilities': ['Fireball'], 'staff': None, 'armour': None}
+        expected = {'name': '', 'position': (1, 1), 'max_hp': 100, 'current_hp': 100, 'xp': 0, 'damage': 20, 'level': 1,
+                    'abilities': ['Fireball'], 'staff': None, 'armour': None}
         self.assertEqual(expected, actual)
 
     def test_make_character_alphabetical(self):
         actual = make_character("Chris")
-        expected = {'name': 'Chris', 'position': (1, 1), 'max_hp': 100, 'current_hp': 100, 'xp': 0, 'damage': 20, 'level': 1, 'abilities': ['Fireball'], 'staff': None, 'armour': None}
+        expected = {'name': 'Chris', 'position': (1, 1), 'max_hp': 100, 'current_hp': 100, 'xp': 0, 'damage': 20,
+                    'level': 1, 'abilities': ['Fireball'], 'staff': None, 'armour': None}
         self.assertEqual(expected, actual)
 
     def test_make_character_numerical(self):
         actual = make_character("1234")
-        expected = {'name': '1234', 'position': (1, 1), 'max_hp': 100, 'current_hp': 100, 'xp': 0, 'damage': 20, 'level': 1, 'abilities': ['Fireball'], 'staff': None, 'armour': None}
+        expected = {'name': '1234', 'position': (1, 1), 'max_hp': 100, 'current_hp': 100, 'xp': 0, 'damage': 20,
+                    'level': 1, 'abilities': ['Fireball'], 'staff': None, 'armour': None}
         self.assertEqual(expected, actual)
 
     def test_make_character_alphanumerical(self):
         actual = make_character("N3p7un3")
-        expected = {'name': 'N3p7un3', 'position': (1, 1), 'max_hp': 100, 'current_hp': 100, 'xp': 0, 'damage': 20, 'level': 1, 'abilities': ['Fireball'], 'staff': None, 'armour': None}
+        expected = {'name': 'N3p7un3', 'position': (1, 1), 'max_hp': 100, 'current_hp': 100, 'xp': 0, 'damage': 20,
+                    'level': 1, 'abilities': ['Fireball'], 'staff': None, 'armour': None}
         self.assertEqual(expected, actual)
